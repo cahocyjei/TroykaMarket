999300a83b9daaa9b0a270cbe624bbf288b414ce
Omar Cabarcas <cahocyjei@gmail.com>
2021-08-23 16:57:46 -0500

    Implementando MapStruct

--- src/main/java/com/troyka/market/persistence/ProductoRepository.java
+++ src/main/java/com/troyka/market/persistence/ProductoRepository.java
@@ -1,10 +1,12 @@
 package com.troyka.market.persistence;
 
 import com.troyka.market.persistence.crud.ProdutoCrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public class ProductoRepository {
 
     private ProdutoCrudRepository produtoCrudRepository;
@@ -20,6 +22,16 @@ public class ProductoRepository {
 
     public Optional<List<Producto>> getEscasos(int cantidad){
         return produtoCrudRepository.findByCantidad_stockLessThanAndEstado(cantidad,true);
+    }
+
+    public Optional<Producto> geProducto(int id_Producto){
+        return produtoCrudRepository.findById(id_Producto);
+    }
+    public Producto save(Producto producto){
+        return produtoCrudRepository.save(producto);
+    }
+    public void delete(int id_Producto){
+        produtoCrudRepository.deleteById(id_Producto);
     }
 
 
---
+++ src/main/java/com/troyka/market/persistence/mapper/ProductMapper.java
@@ -0,0 +1,28 @@
+package com.troyka.market.persistence.mapper;
+
+import com.troyka.market.domain.Product;
+import com.troyka.market.persistence.Producto;
+import org.mapstruct.*;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring", uses = {CategoryMapper.class})
+public interface ProductMapper {
+
+    @Mappings({
+            @Mapping(source= "id_producto",target="productId"),
+            @Mapping(source= "nombre",target = "name"),
+            @Mapping(source= "id_Categoria",target="categoryId"),
+            @Mapping(source= "precioVenta",target = "price"),
+            @Mapping(source= "cantidadStock",target="stock"),
+            @Mapping(source= "estado",target = "active"),
+            @Mapping(source= "categoria",target = "category"),
+    })
+    Product toProduct(Producto producto);
+    List<Product> toProduct(List<Producto> productos);
+
+    @InheritInverseConfiguration
+    @Mapping(target="codigoBarras",ignore = true)
+    Producto toProducto(Product product);
+
+}
