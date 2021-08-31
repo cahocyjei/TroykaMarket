7d90a6adb5522ae16c785c1dea7fb39958829821
Omar Cabarcas <cahocyjei@gmail.com>
2021-08-31 11:30:55 -0500

    Se crea el paquete security para Spring Security

---
+++ src/main/java/com/troyka/market/web/security/JWTUtil.java
@@ -0,0 +1,20 @@
+package com.troyka.market.web.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JWTUtil {
+
+    private static final String KEY= "troykaW1914jjj";
+
+    public String generateToken(UserDetails userDetails){
+        return Jwts.builder().setSubject(userDetails.getUsername()).setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis()+1000*60*60*10))
+                .signWith(SignatureAlgorithm.HS256,KEY).compact();
+    }
+}
