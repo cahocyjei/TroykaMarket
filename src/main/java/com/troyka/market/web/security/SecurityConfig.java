f0e93557ab1c33a895fc0a12c496c65ba82f4d30
Omar Cabarcas <cahocyjei@gmail.com>
2021-09-01 18:53:46 -0500

    Autorizaciones con JWT, WebSecurity

--- src/main/java/com/troyka/market/web/security/SecurityConfig.java
+++ src/main/java/com/troyka/market/web/security/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.troyka.market.web.security;
 
 import com.troyka.market.domain.service.TroykaUserDetailsService;
+import com.troyka.market.web.security.filter.JwtFilterRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -8,12 +9,18 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private TroykaUserDetailsService troykaUserDetailsService;
+
+    @Autowired
+    private JwtFilterRequest jwtFilterRequest;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -22,8 +29,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests().antMatchers("/**/authenticate").permitAll()
-                .anyRequest().authenticated();
+        http.csrf().disable().authorizeRequests().antMatchers("/**/authenticate","/v2/api-docs"
+                ,"/configuration/ui","/swagger-resources/**","/configuration/security"
+                ,"/swagger-ui.html","/webjars/**").permitAll()
+                .anyRequest().authenticated().and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.addFilterBefore(jwtFilterRequest, UsernamePasswordAuthenticationFilter.class);
     }
 
     @Override
